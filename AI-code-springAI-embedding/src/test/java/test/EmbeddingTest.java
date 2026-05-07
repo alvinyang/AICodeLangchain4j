6d1fc39ed8e826fffe09e93e59727fd6121e86c7
alvinyang <yanghuihui8888@163.com>
2026-05-07 16:20:21 +0800

    AI-code-springAI-embedding 文本向量化操作

---
+++ AI-code-springAI-embedding/src/test/java/test/EmbeddingTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import com.ai.code.Application;
+import com.alibaba.cloud.ai.dashscope.embedding.DashScopeEmbeddingModel;
+import org.junit.jupiter.api.Test;
+import org.springframework.ai.embedding.EmbeddingResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootTest(classes = Application.class)
+public class EmbeddingTest {
+
+    @Autowired
+    private DashScopeEmbeddingModel dashScopeEmbeddingModel;
+
+    @Test
+    public void testEmbedding() {
+        String text = "你好";
+        EmbeddingResponse embeddingResponse = dashScopeEmbeddingModel.embedForResponse(List.of(text));
+        float[] output = embeddingResponse.getResult().getOutput();
+        System.out.println(Arrays.toString(output));
+    }
+}
