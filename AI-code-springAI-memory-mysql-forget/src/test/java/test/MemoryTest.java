8da8097a689099010b1439bfc7763855c024a2c0
alvinyang <yanghuihui8888@163.com>
2026-05-07 09:55:36 +0800

    AI-code-springAI 记忆容量控制，与遗忘机制

---
+++ AI-code-springAI-memory-mysql-forget/src/test/java/test/MemoryTest.java
@@ -0,0 +1,62 @@
+package test;
+
+import com.ai.code.Application;
+import org.junit.jupiter.api.Test;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest(classes = Application.class)
+public class MemoryTest {
+
+    @Autowired
+    @Qualifier("memoryChatClient")
+    private ChatClient chatClient;
+
+    @Autowired
+    @Qualifier("digestClient")
+    private ChatClient digestClient;
+
+    @Autowired
+    private ChatMemory chatMemory;
+
+    @Test
+    public void testMemory() {
+        String uuid = "UserId_converSationId";
+        System.out.println(chatClient.prompt().
+                advisors(advisorSpec -> advisorSpec.param(ChatMemory.CONVERSATION_ID, uuid))
+                .user("我是一名30岁的厨师")
+                .call()
+                .content());
+        System.out.println(chatClient.prompt().
+                advisors(advisorSpec -> advisorSpec.param(ChatMemory.CONVERSATION_ID, uuid))
+                .user("我的职业是什么")
+                .call()
+                .content());
+    }
+
+    @Test
+    public void testForget() {
+        String uuid = "UserId_converSationId";
+        List<Message> messages = chatMemory.get(uuid);
+        if (messages.size() >= 5) {
+            // 大于5条会话时，进行摘要汇总，并把之前的记录删掉，添加摘要信息
+            AssistantMessage assistantMessage= digestClient.prompt()
+                    .advisors(advisorSpec -> advisorSpec.param(ChatMemory.CONVERSATION_ID, uuid))
+                    .call().chatResponse().getResult().getOutput();
+            chatMemory.clear(uuid);
+            chatMemory.add(uuid, assistantMessage);
+        }
+        System.out.println(chatClient.prompt()
+                .advisors(advisorSpec -> advisorSpec.param(ChatMemory.CONVERSATION_ID, uuid))
+                .user("我会不会被AI替代")
+                .call()
+                .content());
+    }
+}
