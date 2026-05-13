ccefe44f638b9768a7d87d2147612e27be3be2f0
alvinyang <yanghuihui8888@163.com>
2026-05-13 12:15:59 +0800

    修改api-key的方式

--- AI-code-springAI-memory-redis/src/main/java/com/ai/code/config/ChatConfig.java
+++ AI-code-springAI-memory-redis/src/main/java/com/ai/code/config/ChatConfig.java
@@ -3,6 +3,8 @@ package com.ai.code.config;
 import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatModel;
 import jakarta.annotation.Resource;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.PromptChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -15,18 +17,25 @@ public class ChatConfig {
     @Resource
     private DashScopeChatModel dashScopeChatModel;
 
-    @Bean
-    public ChatClient chatClient(@Value("classpath:test.st") org.springframework.core.io.Resource resource){
-        PromptTemplate promptTemplate = PromptTemplate.builder()
-                //.template("你的名字叫{name}，是以为{zhiye}工程师")
-                .resource(resource) // 通过读取外部文件加装资源数据
-                .build();
-        HashMap<String, Object> map = new HashMap<>();
-        map.put("zhiye", "java");
-        map.put("name", "张三");
+//    @Bean
+//    public ChatClient chatClient(@Value("classpath:test.st") org.springframework.core.io.Resource resource){
+//        PromptTemplate promptTemplate = PromptTemplate.builder()
+//                //.template("你的名字叫{name}，是以为{zhiye}工程师")
+//                .resource(resource) // 通过读取外部文件加装资源数据
+//                .build();
+//        HashMap<String, Object> map = new HashMap<>();
+//        map.put("zhiye", "java");
+//        map.put("name", "张三");
+//        return ChatClient.builder(dashScopeChatModel)
+//                //.defaultSystem("你的名字叫小白，是以为Java工程师")
+//                .defaultSystem(promptTemplate.render(map)) // 使用模板提示词
+//                .build();
+//    }
+
+    @Bean(value = "memoryChatClient")
+    public ChatClient chatClient(ChatMemory chatMemory){
         return ChatClient.builder(dashScopeChatModel)
-                //.defaultSystem("你的名字叫小白，是以为Java工程师")
-                .defaultSystem(promptTemplate.render(map)) // 使用模板提示词
+                .defaultAdvisors(PromptChatMemoryAdvisor.builder(chatMemory).build())
                 .build();
     }
 }
--- AI-code-springAI-memory-redis/src/test/java/test/MemoryTest.java
+++ AI-code-springAI-memory-redis/src/test/java/test/MemoryTest.java
@@ -4,12 +4,14 @@ import com.ai.code.Application;
 import com.ai.code.config.RedisChatMemoryConfig;
 import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatModel;
 import org.junit.jupiter.api.Test;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.messages.AssistantMessage;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.client.ClientHttpRequestFactories;
 import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
@@ -28,6 +30,10 @@ public class MemoryTest {
 
     @Autowired
     private ChatMemory redisChatMemory;
+
+    @Autowired
+    @Qualifier("memoryChatClient")
+    private ChatClient chatClient;
 
     @Test
     public void testMemory(){
@@ -48,5 +54,23 @@ public class MemoryTest {
                 .getResult().getOutput().getText();
         redisChatMemory.add(uuid, new AssistantMessage(msg02));// 记录大模型返回的内容
         System.out.println("第二次会话：" + msg02);
+    }
+
+    /**
+     * 使用client调用
+     */
+    @Test
+    public void testMemory02() {
+        String uuid = "UserId_converSationId";
+        System.out.println(chatClient.prompt().
+                advisors(advisorSpec -> advisorSpec.param(ChatMemory.CONVERSATION_ID, uuid))
+                .user("我是一名30岁的厨师")
+                .call()
+                .content());
+        System.out.println(chatClient.prompt().
+                advisors(advisorSpec -> advisorSpec.param(ChatMemory.CONVERSATION_ID, uuid))
+                .user("我的职业是什么")
+                .call()
+                .content());
     }
 }
